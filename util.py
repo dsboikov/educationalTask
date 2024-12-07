c2e38d2a3bb701b788c907a67d6ac884a268c1d1
Denis <dsboikov@yandex.ru>
2024-12-07 20:37:21 +0300

    Убрал Dialog

--- util.py
+++ util.py
@@ -112,10 +112,3 @@ async def default_callback_handler(update: Update,
     query = update.callback_query.data
     await send_html(update, context, f'You have pressed button with {query} callback')
 
-
-class Dialog:
-    def __init__(self):
-        self.user = {}
-        self.count = 0
-        self.list = []
-        self.mode = None
